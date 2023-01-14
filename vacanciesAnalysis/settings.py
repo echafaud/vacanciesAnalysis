ea72104928bf4e8cf266de460951ee7ce71e75fc
echafaud <103134410+echafaud@users.noreply.github.com>
2023-01-14 23:05:51 +0500

    update settings

--- vacanciesAnalysis/settings.py
+++ vacanciesAnalysis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+r&%kz()-v_--$!k!r!t%j1dvy#xne)^%u#kj!msgbdvx*5str
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['echafaud.pythonanywhere.com']
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'vacanciesAnalysisApp/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
