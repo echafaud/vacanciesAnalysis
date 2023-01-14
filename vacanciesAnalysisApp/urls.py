e55baeeabc5632fe72d3346dc1a6e4968f5fb413
echafaud <103134410+echafaud@users.noreply.github.com>
2023-01-14 11:51:24 +0500

    add bureger links and update header

--- vacanciesAnalysisApp/urls.py
+++ vacanciesAnalysisApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.getIndexPage),
-    path('<page>', views.getPage)
+    path('', views.getIndexPage, name = 'indexPage'),
+    path('<page>', views.getPage, name = 'statisticPage')
 ]
