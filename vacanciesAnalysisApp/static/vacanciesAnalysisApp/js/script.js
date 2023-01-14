eedbf9210b1adea5480fb327db6f56929a23714f
echafaud <103134410+echafaud@users.noreply.github.com>
2023-01-14 18:26:38 +0500

    fix change brightness when open menu

--- vacanciesAnalysisApp/static/vacanciesAnalysisApp/js/script.js
+++ vacanciesAnalysisApp/static/vacanciesAnalysisApp/js/script.js
@@ -2,12 +2,14 @@ function openNav() {
   document.querySelector(".nav").style.width = "300px";
   document.querySelector(".main__container").style.marginLeft = "300px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
-  document.body.style.filter = "brightness(50%)";
+  document.querySelector(".imgs").classList.toggle("brightnessOn")
+
 }
 
 function closeNav() {
   document.querySelector(".nav").style.width = "0";
   document.querySelector(".main__container").style.marginLeft = "0px";
-  document.body.style.filter = "brightness(100%)";
   document.body.style.backgroundColor = "white";
+  document.querySelector(".imgs").classList.remove("brightnessOn")
+
 }
